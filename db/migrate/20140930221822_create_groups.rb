f9c02c5e5179f4fe5954cf5c838349e8b703be31
Dan Hoerr <dan.hoerr@appdirect.com>
2014-09-30 18:35:16 -0400

    added groups

---
+++ db/migrate/20140930221822_create_groups.rb
@@ -0,0 +1,13 @@
+class CreateGroups < ActiveRecord::Migration
+  def change
+    create_table :groups do |t|
+      t.string :name,              :null => false, :default => ""
+      t.timestamps
+    end
+
+    create_table :groups_users, id: false do |t|
+      t.integer :group_id
+      t.integer :user_id
+    end
+  end
+end
